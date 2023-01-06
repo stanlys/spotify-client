54059aa86134d651d8bbca9d7865c206a2c1a8a5
stanlys <stanlys@yandex.ru>
2023-01-06 23:11:01 +0300

    feat: play audio

--- MOCK_DATA/tracks.ts
+++ MOCK_DATA/tracks.ts
@@ -4,7 +4,8 @@ export const TRACKS: ITrack[] = [
   {
     _id: "1",
     artist: "Мара",
-    audio: "1",
+    audio:
+      "http://localhost:3005/audio/36c3c2cf-ee2f-4933-ba4d-1bdaae155d01.mp3",
     picture: "1.jpg",
     text: "1",
     comments: [],
--- components/TrackList/PlayButton.tsx
+++ components/TrackList/PlayButton.tsx
@@ -1,21 +1,32 @@
 import { IconButton } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 import PlayCircle from "@mui/icons-material/PlayCircle";
 import PauseCircleIcon from "@mui/icons-material/PauseCircle";
 import { useAction } from "../../hooks/useAction";
+import { TRACKS } from "../../MOCK_DATA/tracks";
 
 interface PlayButtonProps {
   isPlay: boolean;
 }
+let audio: HTMLAudioElement | null;
 
 const PlayButton: React.FC<PlayButtonProps> = ({ isPlay }) => {
   const { pauseTrack, playTrack } = useAction();
+
+  useEffect(() => {
+    if (!audio) {
+      audio = new Audio(TRACKS[0].audio);
+      // audio.src = ;
+    }
+  }, []);
+
   const play = () => {
-    console.log("click on button", isPlay);
     if (isPlay) {
       playTrack();
+      if (audio) audio.play();
     } else {
       pauseTrack();
+      if (audio) audio.pause();
     }
   };
 
